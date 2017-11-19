4d1e1183026dcab33e39e35efc807acb74521412
Andreas Hoffmeyer <hallo@andreas-hoffmeyer.me>
2017-11-19 18:49:09 +0100

    Fix typo

--- src/components/config/SetTime.vue
+++ src/components/config/SetTime.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="setTime">
-        <h1>Time Set to {{ currentTime }} minutes</h1>
+        <h1>Time set to {{ currentTime }} minutes</h1>
         <label for="time">Select your timer duration
             <select id="time" class="custom-select" v-model.bind="time" @change="setTime(time)">
                 <!-- inline object literal -->
